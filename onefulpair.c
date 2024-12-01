e291f7312c4b8c8022a0e3f8e5977ffb7d6d631b
Rithika P <166213945+Rithika2406@users.noreply.github.com>
2024-12-01 14:19:53 +0530

    Add files via upload

---
+++ onefulpair.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    // Your code goes here (if needed)
+    int n=a+b+(a*b);
+    if(n=111)
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+}
